3264c7f3255df801a36aeb8ddcf8d1974b1de6cc
Trielawr <trielawr@gmail.com>
2025-05-23 01:09:56 +0300

    Continued with main header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
+import { useFormik } from "formik";
 import '../Header/Header.scss';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Icon from '../Icon/Icon';
 import { NavLink } from 'react-router-dom';
 import { ROUTES } from '../../assets/utilits';
+import { validationSchema } from '../../assets/utilits';
 
 const Header = () => {
+
+  const [find, setFind] = useState('');
+  const [disabled, setDisabled] = useState('false');
+  
+  const onChangeForm = () => {
+    setFind('');
+    setDisabled('true');
+  }
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      search: find || ""
+    },
+    validationSchema: validationSchema,
+    onSubmit: () => {
+      onChangeForm();
+      formik.resetForm();
+    }
+  }
+  );
+ 
   return (  
       <header > 
         <div className='header-back'>
@@ -32,15 +56,30 @@ const Header = () => {
               <div className='header-main'>
                 <NavLink to={ROUTES.home} className='header-main-logo'>Exclusive</NavLink>
                 <nav className='header-main-nav'>
-                  <NavLink to={ROUTES.home}>Home</NavLink>
-                  <NavLink to={ROUTES.contact}>Contact</NavLink>
-                  <NavLink to={ROUTES.about}>About</NavLink>
-                  <NavLink to={ROUTES.login}>Sign up</NavLink>
+                  <ul className='header-main-link'>
+                    <li><NavLink to={ROUTES.home}>Home</NavLink></li>
+                    <li><NavLink to={ROUTES.contact}>Contact</NavLink></li>
+                    <li><NavLink to={ROUTES.about}>About</NavLink></li>
+                    <li><NavLink to={ROUTES.login}>Sign up</NavLink></li>
+                  </ul> 
                 </nav>
                 <div className='header-main-form'>
-                  <form className='header-main-search'>
-                    <label></label>
-                    <input type='text'></input>
+                  <form onSubmit={ formik.handleSubmit } className='header-main-search'>
+                    <input
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      name= "search"
+                      type='search'
+                      value={ formik.values.find }
+                      placeholder='What are you looking for?' />
+                      {/* <Button
+                        className='yellow-btn'
+                        text='sent'
+                        type='submit'
+                        disabled={ formik.errors.find }
+                    /> */}
+                        { (formik.errors.find && formik.touched.find) ? <p className='errors'>{ formik.errors.find } </p>: null }
+                    {/* <input type='text' placeholder='What are you looking for?'/> */}
                   </form>
                   <Icon iconname='heart small' width={'32'} height={'32'} />
                   <Icon iconname='Cart1' width={'32'} height={'32'}/>
--- src/assets/utilits.js
+++ src/assets/utilits.js
@@ -1,3 +1,13 @@
+import * as Yup from "yup";
+
+export const validationSchema = Yup.object().shape({
+    query: Yup.string()
+      .trim()
+      .required('Search input is required.')
+      .min(3, 'Search input must be at least 3 characters.')
+      .max(50, 'Search input must not exceed 50 characters.')
+  });
+
 export const ROUTES = {
     home: "/",
     login: "/login",
