0fe6176af7885e298b7aaa4fe05bf4a059b202fe
Trielawr <trielawr@gmail.com>
2025-06-12 21:50:40 +0300

    Finished footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,19 +1,13 @@
-import React from 'react';
 import '../Footer/Footer.scss';
 import { NavLink } from 'react-router-dom';
 import { ROUTES, validationSchemaEmail } from '../../assets/utilits';
 import { footerData } from '../../Date/webdata';
 import Icon from '../Icon/Icon';
-import Button from '../Button/Button';
 import FooterLinkList from './FooterLinkList';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import FieldForm from '../FieldForm/FieldForm';
-import imgAS from '../../assets/img/download-appstore.png';
-import imgGP from '../../assets/img/download-googleplay.png';
-import imgCode from '../../assets/img/Qr Code.png';
-
 
 const Footer = () => {
   return (
@@ -21,8 +15,8 @@ const Footer = () => {
       <div className='container'>
         <ul className='footer-list p-0 m-0'>
           <Container >
-            <Row className="justify-content-md-between" >
-              <Col>
+            <Row className="footer-list-row" >
+              <Col className="footer-list-col col-auto">
                 <li className='footer-list-logo'><NavLink to={ROUTES.home} >Exclusive</NavLink></li>
                 <li className='footer-list-subscribe'><NavLink to={ROUTES.home} >Subscribe</NavLink></li>
                 <li className='footer-list-discount'><NavLink to={ROUTES.home} >Get 10% off your first order</NavLink></li> 
@@ -37,63 +31,16 @@ const Footer = () => {
               </Col>
               {
                 footerData.map((item, index) => 
-                  <Col>
-                    <li key={ index }>
-                      <h5 className='footer-list-title'>{item.title}</h5>
-                      <FooterLinkList item={ item.item }/> 
-                    </li>
+                  <Col className="footer-list-col col-auto">
+                    <div className="footer-list-center">
+                      <li key={ index }>
+                        <h5 className='footer-list-title'>{item.title}</h5>
+                        <FooterLinkList item={ item.item }/> 
+                      </li>
+                    </div>
                   </Col>        
                 )
               }   
-              {/* <Col>
-                <li className='footer-qr'>
-                  <img src={imgCode} alt='QR Code' />
-                </li>
-                <li className='footer-download'>
-                  <div className='footer-download-img'>
-                    <Button
-                        type={'link'}
-                        href={'https://www.apple.com/app-store/'}
-                        text={<img src={imgAS} alt='App Store' />}
-                    />
-                    <Button
-                        type={'link'}
-                        href={'https://play.google.com/store/games?device=windows'}
-                        text={<img src={imgGP} alt='Google Play' />}
-                    /> 
-                  </div>
-                </li>
-              </Col> */}
-              {/* <Col>
-                <li>
-                  <Button
-                        type={'link'}
-                        href={'https://www.facebook.com/'}
-                        text={<Icon iconname='Icon-Facebook' width={'32'} height={'32'} />}
-                  />
-                </li>
-                <li>
-                  <Button
-                        type={'link'}
-                        href={'https://x.com/'}
-                        text={<Icon iconname='Icon-Twitter' width={'32'} height={'32'} />}
-                  />
-                </li>
-                <li>
-                <Button
-                        type={'link'}
-                        href={'https://www.instagram.com/'}
-                        text={<Icon iconname='icon-instagram' width={'32'} height={'32'} />}
-                  />
-                </li>
-                <li>
-                <Button
-                        type={'link'}
-                        href={'https://www.linkedin.com/'}
-                        text={<Icon iconname='Icon-Linkedin' width={'32'} height={'32'} />}
-                  />
-                </li>
-              </Col> */}
             </Row>
           </Container>
         </ul>
