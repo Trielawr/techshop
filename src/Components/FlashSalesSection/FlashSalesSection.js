e74a1d7c7ba6b66fc03ec7b6fe8264c8f7aee952
Trielawr <trielawr@gmail.com>
2025-06-27 00:57:12 +0300

    Made carusel of product cards

--- src/Components/FlashSalesSection/FlashSalesSection.js
+++ src/Components/FlashSalesSection/FlashSalesSection.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React from 'react';
 import CategoryDivider from '../CategoryDivider/CategoryDivider';
 import CountdownTimer from '../CountdownTimer/CountdownTimer';
 import ProductCard from '../ProductCard/ProductCard';
+import Icon from '../Icon/Icon';
+import Slider from "react-slick";
 import './FlashSalesSection.scss'
 
 const FlashSalesSection = () => {
+
+  function NextArrow(props) {
+    const { className, onClick } = props;
+    return (
+      <button
+        type="button"
+        className={className}
+        onClick={onClick}
+      >
+        <Icon iconname="icons arrow-right" width={'24'} height={'24'}/>
+      </button>
+    );
+  }
+  
+  function PrevArrow(props) {
+    const { className, onClick } = props;
+    return (
+      <button
+        type="button"
+        className={className}
+        onClick={onClick}
+      >
+        <Icon iconname="icons_arrow-left" width={'24'} height={'24'}/>
+      </button>
+    );
+  }
+  
+    const settings = {
+      dots: true,
+      infinite: true,
+      dots: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      swipeToSlide: true,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />
+    };
+
   return (
     <div className='container'>
       <div className='flash-sales'>
@@ -14,13 +54,76 @@ const FlashSalesSection = () => {
         </div>
         <CountdownTimer targetDate='2025-07-31T23:59:59'/>
       </div>
-      <ProductCard
-        text='HAVIT HV-G92 Gamepad'
-        sale={'40'}
-        price={'160'}
-        iconname={'Five star'}
-        imgname={'havit'}
-        reviews={'88'}/>
+      <div className="slider-container flash-slider">
+        <Slider {...settings}>
+          <div>
+            <h3>
+              <ProductCard
+                text='HAVIT HV-G92 Gamepad'
+                sale={'40'}
+                price={'160'}
+                iconname={'Five star'}
+                imgname={'havit'}
+                reviews={'88'}/>
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <ProductCard
+                text='AK-900 Wired Keyboard'
+                sale={'35'}
+                price={'1160'}
+                iconname={'Four Star'}
+                imgname={'wired keyboard'}
+                reviews={'75'}/>
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <ProductCard
+                  text='IPS LCD Gaming Monitor'
+                  sale={'35'}
+                  price={'400'}
+                  iconname={'Five star'}
+                  imgname={'gaming monitor'}
+                  reviews={'99'}/>
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <ProductCard
+                    text='S-Series Comfort Chair'
+                    sale={'25'}
+                    price={'400'}
+                    iconname={'Four Half Star'}
+                    imgname={'comfort chair'}
+                    reviews={'99'}/>
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <ProductCard
+                  text='HAVIT HV-G92 Gamepad'
+                  sale={'40'}
+                  price={'160'}
+                  iconname={'Five star'}
+                  imgname={'havit'}
+                  reviews={'88'}/>
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <ProductCard
+                  text='AK-900 Wired Keyboard'
+                  sale={'35'}
+                  price={'1160'}
+                  iconname={'Four Star'}
+                  imgname={'wired keyboard'}
+                  reviews={'75'}/>
+            </h3>
+          </div>
+        </Slider>
+      </div>
     </div>
   )
 }
--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -4,37 +4,35 @@ import Icon from '../Icon/Icon';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-const ProductCard = ({ text, sale, price, iconname, imgname, reviews}) => {
-    console.log('sale', sale/100);
-    console.log('price', price);
-    const newprice = price-price * (sale / 100);
-    console.log('newprize', newprice);
-    console.log('imgname', imgname);
+const ProductCard = ({ text, sale, price, iconname, imgname, reviews }) => {
+  
+  let newprice = price - price * (sale / 100);
+
   return (
       <div className='product-card'>
       <Card style={{ width: '250px' }}>
         <div className='product-card-img'>
-          <Card.Img variant="top" src={`/img/${imgname}.png`} alt={`${imgname}`}/>
+          <Card.Img className='img-fluid' variant="top" src={`/img/${imgname}.png`} alt={`${imgname}`}/>
           {sale && <p className='sale'>{'-' + sale + '%'}</p> }
-          <div className='product-card-icons'>
-            <Icon  iconname={'Fill Heart'} width={'34'} height={'34'}/>
-            <Icon iconname={'Fill Eye'} width={'34'} height={'34'}/>
-          </div>
+          <ul className='product-card-icons'>
+            <li className='product-card-hearticon'><Icon iconname={'Fill Heart'} width={'34'} height={'34'}/></li>
+            <li className='product-card-eyeicon'><Icon iconname={'Fill Eye'} width={'34'} height={'34'}/></li>
+          </ul>
           <Button className='hidden-button' variant="primary">Add To Cart</Button>
         </div>
           <Card.Body>
             <Card.Title>{text}</Card.Title>
-          <Card.Text>
-            <div className='product-card-price'>
-              <p className='product-card-newprice'>{`$` + newprice}</p>
-              <p className='product-card-oldprice'>{`$` + price + '   '}</p>
-            </div>
-            <div className='product-card-reviews'>
-              <Icon iconname={iconname} width={'100'} height={'20'}></Icon>
-              <span>{'(' + reviews + ')'}</span>
-            </div>
-            </Card.Text> 
-        </Card.Body>
+            <Card.Text>
+              <div className='product-card-price'>
+                <p className={ sale ? 'product-card-newprice' : 'product-card-noprice' }>{`$` + newprice}</p>
+                <p className={ sale ? 'product-card-oldprice' : 'product-card-regularprice' }>{`$` + price + '   '}</p>
+              </div>
+              <div className='product-card-reviews'>
+                <Icon iconname={iconname} width={'100'} height={'20'}></Icon>
+                <span>{'(' + reviews + ')'}</span>
+              </div>
+           </Card.Text> 
+          </Card.Body>
         </Card>
     </div>
   )
